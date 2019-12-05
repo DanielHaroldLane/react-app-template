f19ae0f5c7944d51c1a57d1d7f279d43280c861c
Daniel Lane <daniel@application-developer.org>
2019-12-05 02:51:28 +0000

    Add styled components

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-const style = {
-  padding: '5px 10px',
-  border: '1px solid',
-  borderColor: '#34343434',
-  textAlign: 'center',
-  borderRadius: '4px',
-  cursor: 'pointer',
-  userSelect: 'none',
-};
+const Styled = styled.div`
+  padding: 5px 10px;
+  border: 1px solid;
+  border-color: #34343434;
+  text-align: center;
+  border-radius: 4px;
+  cursor: pointer;
+  user-select: none;
+`;
 
 const Button = props => {
   const { text, onClick } = props;
 
-  return (
-    <div style={style} onClick={onClick}>
-      {text}
-    </div>
-  );
+  return <Styled onClick={onClick}>{text}</Styled>;
 };
 
 Button.propTypes = {
